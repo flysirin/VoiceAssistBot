886f9a08a6d9de2c09457a3c7f5f8e55b9f6ef4e
Stanislaus <flysirin@gmail.com>
2023-04-22 22:26:47 +0400

    add handler button_read_text_press

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,11 +2,15 @@
 import asyncio
 
 from aiogram import Router, F, Bot
-from aiogram.filters import Command, CommandStart
+from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
 from lexicon.lexicon import LEXICON
 from services import convert_audio, ai_service
 from aiogram.types.input_file import FSInputFile
+
+from keyboards import keyboards
+
+from config_data import config
 
 # Initialise router by module level
 router: Router = Router()
@@ -23,6 +27,7 @@ async def process_help_command(message: Message):
 
 
 # Second version func for download sound with read binary and convert to text
+# @router.callback_query(lambda c: True)
 @router.message(F.voice | F.audio)
 async def process_audio_to_text(message: Message, bot: Bot):
     try:
@@ -31,20 +36,28 @@ async def process_audio_to_text(message: Message, bot: Bot):
 
         sound_binary_io = await bot.download(file)
         sound_bytes = sound_binary_io
-        answer_message = await message.reply(text=f"File downloaded successfully: \n{file_name}\n Text decoding in progress",
-                                             disable_notification=True)
+        answer_message = await message.reply(
+            text=f"File downloaded successfully: \n{file_name}\n Text decoding in progress",
+            disable_notification=True)
 
         file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=sound_bytes, file_name=file_name)
 
-        text_path = ai_service.transcribe_audio_to_text (file_bytes, file_name)
+        text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
         send_doc = FSInputFile(text_path)
 
         await bot.delete_message(message.chat.id, answer_message.message_id)
-        await message.answer_document(send_doc)
+        await message.answer_document(send_doc,
+                                      reply_markup=keyboards.create_inline_kb())
 
         convert_audio.delete_temp_files()
 
     except BaseException as e:
         alert_message = await message.reply(text=f"Exception: \n{e}")
-        await asyncio.sleep(10)
+        await asyncio.sleep(60)
         await bot.delete_message(alert_message.chat.id, alert_message.message_id)
+
+
+@router.callback_query(Text(text=['read_text_button_pressed']))
+async def button_read_text_press(callback: CallbackQuery):
+    await callback.message.answer(
+        text=config.CURRENT_TEXT_MESSAGE)
