55bac19dc041896947de543cce266211c784339f
Stanislaus <flysirin@gmail.com>
2023-04-24 14:55:17 +0400

    Add info about Bot functions

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,3 +1,5 @@
 LEXICON: dict[str, str] = {'/start': 'Hello! \n I can translate voice to text.\n'
-                                     'Send me voice or sound file with text\n',
-                           '/help': 'Some help info.', }
+                                     'Send me voice message or sound file and I return You text.\n',
+                           '/help': 'I can decode audio files and voice messages into text. \n'
+                                    'By default, you will receive a text file, You can also click on the button to read it right here. \n'
+                                    'I understand all popular sound formats', }
