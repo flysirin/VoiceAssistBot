b37edd3d8fc0d6a71fc480954db507f1a3096296
Stanislaus <flysirin@gmail.com>
2023-04-24 14:54:09 +0400

    Muved function delete_temp to /services/other_services and create function for split text for correct send messege

---
+++ services/other_services.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import textwrap
+
+
+def delete_temp_files(path: str = "temp"):
+    folder = Path(path)
+    for file in folder.glob("*"):
+        if file.is_file() and file.name != ".gitignore":
+            try:
+                file.unlink()
+            except BaseException as e:
+                print(f"File '{file}', can not delete. \n{e}")
+
+
+def split_text(text: str, max_length: int = 4000):
+    wrapped_text = textwrap.wrap(text=text, width=max_length, break_long_words=False)
+    return wrapped_text
