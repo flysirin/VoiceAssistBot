590f083f50722788bd49322065114eda537c1778
Stanislaus <flysirin@gmail.com>
2023-04-24 21:50:58 +0400

    add fix for Inline buttom and fix some bug decode mp3

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,7 @@
 # import io
 import asyncio
 
+import openai.error
 from aiogram import Router, F, Bot
 from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
@@ -35,11 +36,12 @@ async def process_audio_to_text(message: Message, bot: Bot):
         file_name = f"{message.voice.file_size}.ogg" if message.voice else message.audio.file_name
 
         sound_bytes_io = await bot.download(file)
+        sound_bytes = sound_bytes_io.read()
         answer_message = await message.reply(
             text=f"File downloaded successfully: \n{file_name}\n Text decoding in progress",
             disable_notification=True)
 
-        file_bytes = convert_audio.convert_audio_to_mp3(file_bytes_io=sound_bytes_io, file_name=file_name)
+        file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=sound_bytes, file_name=file_name)
 
         text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
         send_doc = FSInputFile(text_path)
@@ -49,15 +51,27 @@ async def process_audio_to_text(message: Message, bot: Bot):
                                       reply_markup=keyboards.create_inline_kb())
 
         other_services.delete_temp_files()
+    except openai.error.InvalidRequestError as e:
+        if "Invalid file format" in str(e):
+            try:
+                file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=file_bytes, file_name=file_name, speed=1)
+                text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
+                send_doc = FSInputFile(text_path)
+                await message.answer_document(send_doc,
+                                              reply_markup=keyboards.create_inline_kb())
+            except BaseException as e:
+                await message.reply(text=f"Exception: \n{e}")
 
     except BaseException as e:
-        alert_message = await message.reply(text=f"Exception: \n{e}, {e.args = }")
+        alert_message = await message.reply(text=f"Exception: \n{e}")
         await asyncio.sleep(60)
         await bot.delete_message(alert_message.chat.id, alert_message.message_id)
 
 
 @router.callback_query(Text(text=['read_text_button_pressed']))
 async def button_read_text_press(callback: CallbackQuery, bot: Bot):
+    await callback.answer()
+
     file = callback.message.document
     file_bytes_io = await bot.download(file)
     text_str = file_bytes_io.read().decode('utf-8')
@@ -66,3 +80,4 @@ async def button_read_text_press(callback: CallbackQuery, bot: Bot):
     for chunk_text in split_text:
         await callback.message.answer(text=chunk_text)
 
+    await callback.answer()
