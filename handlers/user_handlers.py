673888c1c6fd4e3fee6190bd42951c63cc780d47
Stanislaus <flysirin@gmail.com>
2023-04-24 14:57:16 +0400

    Function button_read_text_press now work corect

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -5,7 +5,7 @@ from aiogram import Router, F, Bot
 from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
 from lexicon.lexicon import LEXICON
-from services import convert_audio, ai_service
+from services import convert_audio, ai_service, other_services
 from aiogram.types.input_file import FSInputFile
 
 from keyboards import keyboards
@@ -34,13 +34,12 @@ async def process_audio_to_text(message: Message, bot: Bot):
         file = message.voice if message.voice else message.audio
         file_name = f"{message.voice.file_size}.ogg" if message.voice else message.audio.file_name
 
-        sound_binary_io = await bot.download(file)
-        sound_bytes = sound_binary_io
+        sound_bytes_io = await bot.download(file)
         answer_message = await message.reply(
             text=f"File downloaded successfully: \n{file_name}\n Text decoding in progress",
             disable_notification=True)
 
-        file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=sound_bytes, file_name=file_name)
+        file_bytes = convert_audio.convert_audio_to_mp3(file_bytes_io=sound_bytes_io, file_name=file_name)
 
         text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
         send_doc = FSInputFile(text_path)
@@ -49,15 +48,21 @@ async def process_audio_to_text(message: Message, bot: Bot):
         await message.answer_document(send_doc,
                                       reply_markup=keyboards.create_inline_kb())
 
-        convert_audio.delete_temp_files()
+        other_services.delete_temp_files()
 
     except BaseException as e:
-        alert_message = await message.reply(text=f"Exception: \n{e}")
+        alert_message = await message.reply(text=f"Exception: \n{e}, {e.args = }")
         await asyncio.sleep(60)
         await bot.delete_message(alert_message.chat.id, alert_message.message_id)
 
 
 @router.callback_query(Text(text=['read_text_button_pressed']))
-async def button_read_text_press(callback: CallbackQuery):
-    await callback.message.answer(
-        text=config.CURRENT_TEXT_MESSAGE)
+async def button_read_text_press(callback: CallbackQuery, bot: Bot):
+    file = callback.message.document
+    file_bytes_io = await bot.download(file)
+    text_str = file_bytes_io.read().decode('utf-8')
+
+    split_text = other_services.split_text(text_str)
+    for chunk_text in split_text:
+        await callback.message.answer(text=chunk_text)
+
