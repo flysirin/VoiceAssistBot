86c9457f4e9bf9dc88dfe987abdb71cc425d897a
Stanislaus <flysirin@gmail.com>
2023-04-24 21:57:39 +0400

    add default param speed, func now take bytes and not bytes_io

--- services/convert_audio.py
+++ services/convert_audio.py
@@ -3,29 +3,26 @@ import subprocess
 from config_data.config import FFMPEG_BINARY
 
 
-def convert_audio_to_mp3(file_bytes_io: io.BytesIO = None,
+def convert_audio_to_mp3(file_bytes: bytes = None,
                          path_input=None,
-                         file_name='default_name') -> bytes | None:
-    speed = 1.4
-    bitrate = "64k"
+                         file_name='default_name',
+                         speed=1.4) -> bytes | None:
+    bitrate = "33k"
 
     if path_input or file_name.split(".")[-1] == "aac":
         if not path_input:
             path_input = f"temp/{file_name}"
             with open(path_input, "wb") as temp_file:
-                temp_file.write(file_bytes_io.read())
+                temp_file.write(file_bytes)
 
         command = [FFMPEG_BINARY, "-i", path_input, "-b:a", bitrate, "-filter:a",
                    f"atempo={speed}", "-f", "mp3", "pipe:"]
         output_data = subprocess.run(command, stdout=subprocess.PIPE, check=True)
         return output_data.stdout
 
-    if file_bytes_io:
+    if file_bytes:
         command = [FFMPEG_BINARY, "-i", "pipe:", "-b:a", bitrate,
                    "-filter:a", f"atempo={speed}", "-f", "mp3", "pipe:"]
-        file_bytes = file_bytes_io.read()  # Read BytesIO obj
+        # file_bytes = file_bytes_io  # Read BytesIO obj
         output_data = subprocess.run(command, input=file_bytes, stdout=subprocess.PIPE, check=True)
         return output_data.stdout
-
-
-
