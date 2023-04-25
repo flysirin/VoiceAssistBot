efb73e3b2de48e202532f3af69eadf16bf02e8d5
Stanislaus <flysirin@gmail.com>
2023-04-26 01:14:58 +0400

    Added file type check to try to send AI directly and download size check

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,7 +1,6 @@
-# import io
 import asyncio
+import subprocess
 
-import openai.error
 from aiogram import Router, F, Bot
 from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
@@ -12,7 +11,7 @@ from aiogram.types.input_file import FSInputFile
 from keyboards import keyboards
 
 from config_data import config
-
+from random import choice
 # Initialise router by module level
 router: Router = Router()
 
@@ -33,7 +32,10 @@ async def process_help_command(message: Message):
 async def process_audio_to_text(message: Message, bot: Bot):
     try:
         file = message.voice if message.voice else message.audio
-        file_name = f"{message.voice.file_size}.ogg" if message.voice else message.audio.file_name
+        file_name = f"{message.voice.file_unique_id}.ogg" if message.voice else message.audio.file_name
+        if file.file_size > 20.8e6:
+            return await message.reply(
+                text=f"Sound file is too big. \nPlease use files less then 20Mb")
 
         sound_bytes_io = await bot.download(file)
         sound_bytes = sound_bytes_io.read()
@@ -41,9 +43,11 @@ async def process_audio_to_text(message: Message, bot: Bot):
             text=f"File downloaded successfully: \n{file_name}\n Text decoding in progress",
             disable_notification=True)
 
-        file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=sound_bytes, file_name=file_name)
+        if file.file_size > 25e6 or file_name.split(".")[-1] not in ['mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav',
+                                                                     'webm']:
+            sound_bytes = convert_audio.convert_audio_to_mp3(file_bytes=sound_bytes, file_name=file_name)
 
-        text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
+        text_path = ai_service.transcribe_audio_to_text(sound_bytes, file_name)
         send_doc = FSInputFile(text_path)
 
         await bot.delete_message(message.chat.id, answer_message.message_id)
@@ -51,21 +55,14 @@ async def process_audio_to_text(message: Message, bot: Bot):
                                       reply_markup=keyboards.create_inline_kb())
 
         other_services.delete_temp_files()
-    except openai.error.InvalidRequestError as e:
-        if "Invalid file format" in str(e):
-            try:
-                file_bytes = convert_audio.convert_audio_to_mp3(file_bytes=file_bytes, file_name=file_name, speed=1)
-                text_path = ai_service.transcribe_audio_to_text(file_bytes, file_name)
-                send_doc = FSInputFile(text_path)
-                await message.answer_document(send_doc,
-                                              reply_markup=keyboards.create_inline_kb())
-            except BaseException as e:
-                await message.reply(text=f"Exception: \n{e}")
+
+    except subprocess.SubprocessError:
+        await message.reply(text=f"{choice(LEXICON['wrong_decode'])}")
 
     except BaseException as e:
-        alert_message = await message.reply(text=f"Exception: \n{e}")
-        await asyncio.sleep(60)
-        await bot.delete_message(alert_message.chat.id, alert_message.message_id)
+        alert_message = await message.reply(text=f"{choice(LEXICON['another_wrong'])} \n{e}")
+        # await asyncio.sleep(60)
+        # await bot.delete_message(alert_message.chat.id, alert_message.message_id)
 
 
 @router.callback_query(Text(text=['read_text_button_pressed']))
@@ -81,3 +78,11 @@ async def button_read_text_press(callback: CallbackQuery, bot: Bot):
         await callback.message.answer(text=chunk_text)
 
     await callback.answer()
+
+
+@router.message(F.video)
+async def this_is_video(message: Message, bot: Bot):
+    await message.reply(text=f"file video size: {message.video.file_size}")
+    file = await bot.download(message.video)
+    with open("tests/file.v", "wb") as f:
+        f.write(file.read())
