480d4c5b93107fb45f2bf846ebf425ee1fe708a5
Stanislaus <flysirin@gmail.com>
2023-04-26 20:03:59 +0400

    add first button 'Send text to AI GPT 3.5'

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,8 +5,12 @@ from lexicon.lexicon import LEXICON
 def create_inline_kb(*args, **kwargs) -> InlineKeyboardMarkup:
     read_text_button: InlineKeyboardButton = InlineKeyboardButton(
         text="Press for read text",
-        callback_data='read_text_button_pressed')
+        callback_data="read_text_button_pressed")
+    send_text_open_ai_button: InlineKeyboardButton = InlineKeyboardButton(
+        text="Send text to AI GPT 3.5",
+        callback_data="send_text_open_ai"
+    )
     keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(
-        inline_keyboard=[[read_text_button]])
+        inline_keyboard=[[read_text_button],
+                         [send_text_open_ai_button]])
     return keyboard
-
