cc73978293b44e120ba35988d56ee92e25231d91
Stanislaus <flysirin@gmail.com>
2023-04-30 00:17:27 +0400

    Update lexicon.py

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -4,16 +4,27 @@ LEXICON: dict[str, str] = {
     '/help': 'I can decode audio files and voice messages into text. \n'
              'By default, you will receive a text file, You can also click on the button to read it right here. \n'
              'I understand all popular sound formats',
+
     'another_wrong': [
         "Well, we tried our best to break the world record for procrastination. Almost made it, too.",
         "We put so much effort into that project that we almost broke a sweat. Almost.",
         "We were so close to success, we could practically taste it. Then we realized it was just our lunch.",
         "It's not that we didn't put in any effort, it's just that we didn't put in enough effort to actually succeed.",
         "We tried so hard, and got so far, but in the end, it turns out we were just really bad at it."],
+
     'wrong_decode': [
         "Ah yes, the infamous 'corrupted audio file' - the audio equivalent of a blank sheet of paper.",
         "I tried to open your audio file, but my computer seemed to think it was a poorly written piece of code. Can't say I disagree.",
         "Your audio file seems to have taken a wrong turn somewhere and ended up in the format Bermuda Triangle. May it rest in peace.",
         "I'm sorry, I can't seem to open your audio file. It's like trying to use a key to open a door that's been sealed shut with superglue.",
-        "I was really looking forward to listening to your audio file, but it looks like it suffered the same fate as my hopes and dreams - crushed beyond repair."]
+        "I was really looking forward to listening to your audio file, but it looks like it suffered the same fate as my hopes and dreams - crushed beyond repair."],
+
+    'no_words': [
+        r"`This wordless audio file` can show us that emptiness does not mean the absence of manifestation.",
+        r"`This wordless audio file` reminds us that emptiness is not an empty place, but rather a state of mind free from attachment and limitation.",
+        r"`In this wordless audio file`, we can hear that in the void there is nothing to attach to, but it makes everything possible.",
+        r"`This wordless audio file` helps us understand that emptiness is a state in which there are no boundaries and limitations, and everything is possible."],
+
+    'server_error': [
+        r"`A server error` is not a failure, but an opportunity to improve."],
 }
