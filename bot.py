83bd1918dd55c400b42b1b6950687327f9aa438c
Stanislaus <flysirin@gmail.com>
2023-05-01 13:11:19 +0400

    change  parse_mode=HTML to parse_mode=None, because some answer from GPT Chat can not parse with MarkdownV2 or HTML

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from handlers import admin_handlers, user_handlers, other_handlers
+from handlers import user_handlers #, other_handlers
 
-from config_data.config import OPENAI_API_KEY, OPENAI_API_KEY_FROM_HRY, BOT_TOKEN, ADMIN_IDS
-from services import ai_service, convert_audio
+from config_data.config import BOT_TOKEN
+
 
 # Logger initialise
 logger = logging.getLogger(__name__)
@@ -23,12 +23,12 @@ async def main():
     logger.info("Starting bot")
 
     # Initialise Bot and Dispatcher
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    bot = Bot(token=BOT_TOKEN, parse_mode=None)
     dp = Dispatcher()
 
     # Register routers in Dispatcher
     dp.include_router(user_handlers.router)
-    dp.include_router(other_handlers.router)
+    # dp.include_router(other_handlers.router)
 
     # Pass all updates and run polling
     await bot.delete_webhook(drop_pending_updates=True)
