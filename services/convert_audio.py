aae09c0716ae0847551e46bc98c360813bd92bd5
Stanislaus <flysirin@gmail.com>
2023-05-04 16:27:20 +0400

    update for linux and docker build

--- services/convert_audio.py
+++ services/convert_audio.py
@@ -2,6 +2,7 @@ import subprocess
 from config_data.config import FFMPEG_BINARY_WINDOWS, FFMPEG_BINARY_LINUX
 
 FFMPEG_BINARY = FFMPEG_BINARY_LINUX
+# FFMPEG_BINARY = FFMPEG_BINARY_WINDOWS
 
 
 def convert_audio_to_mp3(file_bytes: bytes = None,
