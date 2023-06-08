8e5195b5058a2498819776627653bd31eec4e262
Stanislaus <flysirin@gmail.com>
2023-06-09 02:38:10 +0400

    Add WEBHOOK_URL Env

--- config_data/config.py
+++ config_data/config.py
@@ -7,6 +7,8 @@ OPENAI_API_KEY = env("OPENAI_API_KEY")
 BOT_TOKEN = env("BOT_TOKEN")
 ADMIN_IDS = env("ADMIN_IDS")
 
+WEBHOOK_URL = env("WEBHOOK_URL")
+
 
 FFMPEG_BINARY_WINDOWS = f"services/ffmpeg_windows/ffmpeg.exe"
 FFMPEG_BINARY_LINUX = f"services/ffmpeg_linux/ffmpeg"
