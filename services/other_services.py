0a4e848e8bbdc5434cf694f5c63fefbf4f207397
Stanislaus <flysirin@gmail.com>
2023-06-22 22:27:34 +0400

    add annotation

--- services/other_services.py
+++ services/other_services.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import textwrap
 
 
-def delete_temp_files(path: str = "temp"):
+def delete_temp_files(path: str = "temp") -> None:
     folder = Path(path)
     for file in folder.glob("*"):
         if file.is_file() and file.name != ".gitignore":
@@ -12,6 +12,6 @@ def delete_temp_files(path: str = "temp"):
                 print(f"File '{file}', can not delete. \n{e}")
 
 
-def split_text(text: str, max_length: int = 4000):
+def split_text(text: str, max_length: int = 4000) -> list[str]:
     wrapped_text = textwrap.wrap(text=text, width=max_length, break_long_words=False)
     return wrapped_text
