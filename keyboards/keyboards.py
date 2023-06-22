438fbef176e8476799750b339227ac3808949c15
Stanislaus <flysirin@gmail.com>
2023-06-22 22:47:42 +0400

    add annotation

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -8,8 +8,7 @@ def create_inline_kb(*args, **kwargs) -> InlineKeyboardMarkup:
         callback_data="read_text_button_pressed")
     send_text_open_ai_button: InlineKeyboardButton = InlineKeyboardButton(
         text="Send text to AI GPT 3.5",
-        callback_data="send_text_open_ai"
-    )
+        callback_data="send_text_open_ai")
     keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(
         inline_keyboard=[[read_text_button],
                          [send_text_open_ai_button]])
